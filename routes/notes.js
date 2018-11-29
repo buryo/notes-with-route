9e9e3d4a68c2100651b6a197de5f300235902468
buryo <buryo95@gmail.com>
2018-11-29 17:47:24 +0100

    added new features

--- routes/notes.js
+++ routes/notes.js
@@ -3,18 +3,30 @@ const express = require('express');
 const router = express.Router();
 const json = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
+// Loop through json array and look for the title
+let getNote = (title) => {
+    return json.find( item => {
+        return item.title === title;
+    });
+};
 
 router.get('/add-note', (req, res, next) => {
     res.render('add-note');
 });
 
 router.post('/add-note', (req, res, next) => {
-    json.push(req.body);
-    fs.writeFileSync('./data.json', JSON.stringify(json));
-    res.redirect('/all-notes');
+    // check if duplicated, so not? we can add the new note
+    if (getNote(req.body.title)){
+        res.send('Note bestaat al!');
+    } else{
+        json.push(req.body);
+        fs.writeFileSync('./data.json', JSON.stringify(json));
+        res.redirect('/all-notes');
+    }
 });
 
 router.get('/all-notes', (req, res, next) => {
+    //Render view with the json array
     res.render('notes', { notes:json });
 });
 
@@ -23,7 +35,11 @@ router.get('/get-note', (req, res, next) => {
 });
 
 router.post('/get-note', (req, res, next) => {
-    console.log('you wanted to look for a note?');
+    if (getNote(req.body.title)){
+        res.render('note', { note:getNote(req.body.title) })
+    } else{
+        res.render('note');
+    }
 });
 
 router.get('/delete-note', (req, res, next) => {
